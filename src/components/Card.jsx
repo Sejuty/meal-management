9b8e8900d80e70e348995ca5884b910c9dad0014
Sejuty <tafannumnishat00@gmail.com>
2024-07-11 23:14:12 +0600

    ui-update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,29 @@
 import PropTypes from "prop-types";
 
-const Card = ({ title, content }) => {
+const Card = ({ title, content, className, ...props }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden bg-gray-800 text-gray-200">
-      <div className="px-10 py-4">
-        <div className="font-normal text-xl ">{title}</div>
-      </div>
-
-      <div className="px-3 pb-4">
-        <p className="text-center">{content}</p>
-      </div>
+    <div
+      className={`bg-gray-800  text-gray-200 shadow-md rounded-lg overflow-hidden p-4 py-6 ${className}`}
+      {...props}
+    >
+      {title && <div className="text-sm text-center mb-3">{title}</div>}
+      {content && (
+        <div className="text-gray-200 text-center text-2xl font-bold ">
+          {content}
+        </div>
+      )}
     </div>
   );
 };
 
 Card.propTypes = {
   title: PropTypes.string,
-  content: PropTypes.string,
+  content: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.node,
+  ]),
+  className: PropTypes.string,
 };
 
 export default Card;
--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -9,23 +9,38 @@ const Dashboard = () => {
   const columns = ["ID", "Name", "Email", "Lunch", "Snack"];
   const menuItems = ["Dashboard", "About", "Services", "Logout"];
 
-  const [data, setData] = useState(user)
+  const [data, setData] = useState(user);
 
   const handleDelete = (id) => {
-    const newData = data.filter((item) => id !==item.id );
+    const newData = data.filter((item) => id !== item.id);
     setData(newData);
   };
-
 
   return (
     <div className="flex">
       <Sidebar menuItems={menuItems} />
       <div className="flex-grow p-4">
-        <h1 className="text-2xl font-bold mb-4">Meal Management</h1>
-        <Table columns={columns} data={data} className="p-5 w-fit" onDelete={handleDelete} />
-        <div className="flex flex-wrap gap-4">
-          <Card title="Lunch" content={numberOfMeal(data, "lunch")} />
-          <Card title="Snack" content={numberOfMeal(data, "snack")} />
+        <h1 className="text-2xl font-bold my-4 text-center text-gray-700">Meal Management</h1>
+        <div className="flex flex-wrap justify-between  gap-10 items-center px-8 py-5">
+          <Table
+            columns={columns}
+            data={data}
+            className="w-full"
+            onDelete={handleDelete}
+          />
+          <div className="flex w-full h-full gap-4">
+            <Card
+              title="Lunch"
+              content={numberOfMeal(data, "lunch")}
+              className="w-1/2 md:w-1/2 p-2"
+            />
+
+            <Card
+              title="Snack"
+              content={numberOfMeal(data, "snack")}
+              className="w-full md:w-1/2 p-2"
+            />
+          </div>
         </div>
       </div>
     </div>
